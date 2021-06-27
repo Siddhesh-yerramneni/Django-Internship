32f2dd529f5fbb061d44b84ce321be3a12292e73
siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2021-06-27 11:19:05 +0530

    Commit

---
+++ Restaurant/Restaurant_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from Restaurant_app import views
+urlpatterns = [
+    path('',views.home,name="hm"),
+    path('abt/',views.about,name="ab"),
+    path('cnct/',views.contact,name="ct"),
+]
